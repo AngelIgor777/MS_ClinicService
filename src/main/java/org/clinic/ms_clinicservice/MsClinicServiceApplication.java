3a0048524574519f30076f1c0438d339deeb2a33
atmosfera <igorangelcev69@gmail.com>
2024-11-04 21:40:42 +0300

    init

---
+++ src/main/java/org/clinic/ms_clinicservice/MsClinicServiceApplication.java
@@ -0,0 +1,13 @@
+package org.clinic.ms_clinicservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClinicServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsClinicServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/clinic/ms_clinicservice/MsClinicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.clinic.ms_clinicservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClinicServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

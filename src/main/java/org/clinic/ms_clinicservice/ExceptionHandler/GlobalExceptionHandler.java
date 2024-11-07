08b22b44ee979ae5bab2bb40734b688a272d4421
atmosfera <igorangelcev69@gmail.com>
2024-11-07 16:22:38 +0300

    настройка ExceptionHandler

--- src/main/java/org/clinic/ms_clinicservice/ExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/org/clinic/ms_clinicservice/ExceptionHandler/GlobalExceptionHandler.java
@@ -7,14 +7,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.HashMap;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<String> handleUnauthorizedException(EntityNotFoundException ex) {
+    public ResponseEntity<HashMap<String, String>> handleUnauthorizedException(EntityNotFoundException ex) {
 //        log.error(ex.getMessage());
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("error-message", ex.getMessage());
+        return new ResponseEntity<>(hashMap, HttpStatus.NOT_FOUND);
     }
 }

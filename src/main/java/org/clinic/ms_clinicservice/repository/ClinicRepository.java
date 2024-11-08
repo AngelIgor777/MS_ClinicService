7b88b9dd97c2a6788709079f1562b0a42bd5a3d7
atmosfera <igorangelcev69@gmail.com>
2024-11-08 11:29:05 +0300

    query запрос

--- src/main/java/org/clinic/ms_clinicservice/repository/ClinicRepository.java
+++ src/main/java/org/clinic/ms_clinicservice/repository/ClinicRepository.java
@@ -2,8 +2,13 @@ package org.clinic.ms_clinicservice.repository;
 
 import org.clinic.ms_clinicservice.entity.Clinic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClinicRepository extends JpaRepository<Clinic, Integer> {
+    boolean existsByName(String name);
+
+    @Query("select case when count(c) > 0 then true else false end from Clinic c where c.name != ?1 and c.id = ?2")
+    boolean notExistsByNameAndExistById(String name,int id);
 }

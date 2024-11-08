ab85bc811cbf172863875f74a727df5c6c9906a9
atmosfera <igorangelcev69@gmail.com>
2024-11-08 11:46:22 +0300

    query запрос

--- src/main/java/org/clinic/ms_clinicservice/service/impl/ClinicServiceImpl.java
+++ src/main/java/org/clinic/ms_clinicservice/service/impl/ClinicServiceImpl.java
@@ -3,10 +3,13 @@ package org.clinic.ms_clinicservice.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.clinic.ms_clinicservice.dto.ClinicDTO;
 import org.clinic.ms_clinicservice.entity.Clinic;
+import org.clinic.ms_clinicservice.exception.EmptyResultException;
 import org.clinic.ms_clinicservice.repository.ClinicRepository;
 import org.clinic.ms_clinicservice.service.ClinicService;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 import java.util.List;
@@ -22,6 +25,10 @@ public class ClinicServiceImpl implements ClinicService {
 
 
     public Clinic createClinic(Clinic clinic) {
+        if (clinicRepository.existsByName(clinic.getName())) {
+            throw new EntityExistsException("Clinic with name " + clinic.getName() + " already exists");
+        }
+
         return clinicRepository.save(clinic);
     }
 
@@ -32,13 +39,15 @@ public class ClinicServiceImpl implements ClinicService {
 
     public List<ClinicDTO> getAllClinics() {
         List<Clinic> all = clinicRepository.findAll();
-        return all.stream()
+        List<ClinicDTO> clinicDTOS = all.stream()
                 .map(mapServiceImpl::mapToClinicDTO).toList();
+
+        return clinicDTOS;
     }
 
 
     public Clinic updateClinic(Integer id, Clinic updatedClinic) {
-        if (clinicRepository.existsById(id)) {
+        if (clinicRepository.notExistsByNameAndExistById(updatedClinic.getName(), id)) {
             updatedClinic.setId(id);
             return clinicRepository.save(updatedClinic);
         }
